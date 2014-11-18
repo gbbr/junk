95cc99dbf7b7be93ef372ff8757f692983ef6afd
gbbr <ga@stripetree.co.uk>
2014-11-18 18:56:54 +0000

    functions

--- thieves.go
+++ thieves.go
@@ -11,89 +11,98 @@ import (
 	"time"
 )
 
-func clear() {
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
+var (
+	thieves      = [...]string{"Mordy", "Jerk", "Jameson", "Janice", "Picasso"}
+	donors       = [...]string{"Jim", "Willy", "Babadook", "Hendrix", "Johnson"}
+	participants = len(thieves) + len(donors)
+)
+
+type box struct {
+	donations int
+	openedBy  string
+	waiting   bool
+}
+
+var (
+	cmds = make(chan func(b *box))
+	die  = make(chan bool, participants)
+)
+
+func exposeBox() {
+	var beat box
+	for {
+		select {
+		case fn := <-cmds:
+			clearScreen()
+			fn(&beat)
+			fmt.Printf("Contents: %+v\n", beat)
+			beat.waiting = false
+		default:
+			if !beat.waiting {
+				log.Println("Waiting...")
+				beat.waiting = true
+			}
+		}
+	}
+}
+
+func donor(num int) {
+DEPOSITS:
+	for {
+		select {
+		case cmds <- func(b *box) {
+			b.donations += num
+			b.openedBy = donors[rand.Intn(5)]
+		}:
+		case <-die:
+			break DEPOSITS
+		}
+		<-time.After(time.Duration(rand.Intn(2)) * time.Second)
+	}
+	fmt.Println("Going home...")
+}
+
+func thief(num int) {
+THEFTS:
+	for {
+		select {
+		case cmds <- func(b *box) {
+			if b.donations-num < 0 {
+				num = b.donations
+			}
+			b.donations -= num
+			b.openedBy = thieves[rand.Intn(5)]
+		}:
+		case <-die:
+			break THEFTS
+		}
+		<-time.After(time.Duration(rand.Intn(2)) * time.Second)
+	}
+	fmt.Println("Busted!")
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	type box struct {
-		items       int
-		lastDeposit string
-		waiting     bool
+	go exposeBox()
+	for i := 0; i < len(donors); i++ {
+		go donor(i)
 	}
-
-	cmds := make(chan func(b *box))
-	go func() {
-		var beat box
-		for {
-			select {
-			case fn := <-cmds:
-				clear()
-				fn(&beat)
-				fmt.Printf("Contents: %+v\n", beat)
-				beat.waiting = false
-			default:
-				if !beat.waiting {
-					log.Println("Waiting...")
-					beat.waiting = true
-				}
-			}
-		}
-	}()
-
-	// Charity workers
-	die := make(chan bool, 9)
-	for i := 0; i < 5; i++ {
-		name := [5]string{"Jim", "Willy", "Babadook", "Hendrix", "Johnson"}
-		go func(num int) {
-		DEPOSITS:
-			for {
-				select {
-				case cmds <- func(b *box) {
-					b.items += num
-					b.lastDeposit = name[rand.Intn(5)]
-				}:
-				case <-die:
-					break DEPOSITS
-				}
-				<-time.After(time.Duration(rand.Intn(2)) * time.Second)
-			}
-			fmt.Println("I'm DEAD")
-		}(i)
-	}
-
-	// Thieves
-	for i := 0; i < 5; i++ {
-		name := [5]string{"Mordy", "Jerk", "Jameson", "Janice", "Picasso"}
-		go func(num int) {
-		DEPOSITS:
-			for {
-				select {
-				case cmds <- func(b *box) {
-					if b.items-num < 0 {
-						num = b.items
-					}
-					b.items -= num
-					b.lastDeposit = name[rand.Intn(5)]
-				}:
-				case <-die:
-					break DEPOSITS
-				}
-				<-time.After(time.Duration(rand.Intn(2)) * time.Second)
-			}
-			fmt.Println("I'm DEAD")
-		}(i)
+	for i := 0; i < len(thieves); i++ {
+		go thief(i)
 	}
 
 	ntr := make(chan os.Signal, 1)
 	signal.Notify(ntr, os.Interrupt)
 	<-ntr
-
-	for i := 0; i < 10; i++ {
+	// Alert participants
+	for i := 0; i < participants; i++ {
 		die <- true
 	}
+}
+
+func clearScreen() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
